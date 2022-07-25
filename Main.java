bb6e1fa1dffce4ba2fb1ce2617a7e0ddc94b8f1e
Nataliya Romanyuk <nataromm1999@gmail.com>
2022-07-25 13:35:19 +0300

    (edited) Homework from Udemy course "Java for Absolute Beginners". Lessons 21-34.

--- Main.java
+++ Main.java
@@ -1,5 +1,13 @@
+import Homework_Excercises.S_L_circle;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hi world!");
+
+        int i;
+        for (i = 1; i < 6; i++) {
+            System.out.println("S of the circle with " + i + " sm as a radius in equal to " + S_L_circle.AreaOfCircle(i));
+            System.out.println("L of the circle with " + i + " sm as a radius in equal to " + S_L_circle.LengthOfCircle(i) + "\n");
+        }
     }
 }
